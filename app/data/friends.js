7736f25ee7f9e500a9e1d49e1c518cfded088db7
Veronica <chingona27>
2018-07-16 22:11:12 -0500

    data update, added more friends

---
+++ app/data/friends.js
@@ -0,0 +1,102 @@
+var friends = [
+    {
+        name:"Spongebob",
+        photo: "http://media.comicbook.com/2017/05/spongebob-994959-1280x0.png",
+        scores: [
+            "5",
+            "1",
+            "4",
+            "4",
+            "5",
+            "1",
+            "2",
+            "5",
+            "4",
+            "1"
+        ]
+    },
+    {
+        name:"Patrick",
+        photo: "https://ih0.redbubble.net/image.524290775.8088/ap,550x550,16x12,1,transparent,t.u7.png",
+        scores: [
+            "4",
+            "2",
+            "5",
+            "1",
+            "3",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2"
+        ]
+    },
+    {
+        name:"Sandy",
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/4/46/Sandy_in_SpongeBob_SquarePants_vs._The_Big_One-1.png/revision/latest?cb=20141213233152",
+        scores: [
+            "5",
+            "2",
+            "2",
+            "2",
+            "4",
+            "1",
+            "3",
+            "2",
+            "5",
+            "5"
+        ]
+    },
+    {
+        name:"Mr.Krabs",
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/9/9f/Krabs_artwork.PNG/revision/latest?cb=20171223013253",
+        scores: [
+            "3",
+            "3",
+            "4",
+            "2",
+            "2",
+            "1",
+            "3",
+            "2",
+            "2",
+            "3"     
+        ] 
+    },
+    {
+        name:"Squidward",
+        photo: "https://imgix.ranker.com/user_node_img/50057/1001120395/original/defended-spongebob-from-a-rude-customer-photo-u1?w=650&q=50&fm=jpg&fit=crop&crop=faces",
+        scores: [
+            "4",
+            "3",
+            "4",
+            "1",
+            "5",
+            "2",
+            "5",
+            "3",
+            "1",
+            "4"
+      
+        ]
+    },
+    {
+        name: "Gary",
+        photo: "https://vignette.wikia.nocookie.net/spongebob/images/8/81/Gary_the_Snail.svg/revision/latest/scale-to-width-down/270?cb=20100922010234",
+        scores: [
+          "4",
+          "4",
+          "2",
+          "3",
+          "2",
+          "2",
+          "3",
+          "2",
+          "4",
+          "5"
+        ]
+    }
+  
+  ];
+  
+  
